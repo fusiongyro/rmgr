d0982f879b384715c77fc9072b7ab40ef5c66a7e
Daniel Lyons <dlyons@nrao.edu>
2017-01-03 08:54:48 -0700

    fiddling with the shell parser, still having issues

--- shell.pl
+++ shell.pl
@@ -1,23 +1,28 @@
-:- module(shell, [main/0]).
-:- use_module(library(dcg/basics)).
+:- module(shell, [main/0, print_recipe/1]).
+:- use_module(library(dcg/basics), except([atom//1])).
 :- use_module(parse).
 
 parse_command(Codes, Command) :-
     phrase(parse_command(Command), Codes).
 
+atom(A) --> string(String), { atom_codes(A, String) }.
+
 parse_command(load(Filename)) -->
-    "l", whites, string(StringFilename), { atom_codes(Filename, StringFilename) }.
+    "l", whites, atom(Filename).
 %parse_command(add_note(Note)) -->
 %    "n", whites, string(Note).
 parse_command(add_ingredient(i(Qty, Unit, Name))) -->
-    "a", whites, number(Qty), whites, string(Unit), whites, string(Name).
+    "a", whites, number(Qty), whites, atom(Unit), whites, atom(Name).
 parse_command(change_ingredient(i(Qty, Unit, Name))) -->
-    "c", whites, number(Qty), whites, string(Unit), whites, string(Name).
+    "c", whites, number(Qty), whites, atom(Unit), whites, atom(Name).
+parse_command(Text, [], not_understood(Text)).
 
 execute_command(_, load(Filename), ready(loaded(Filename), Recipe)) :-
     load_recipe(Filename, Recipe).
+execute_command(State, not_understood(Text), State) :-
+    format('Error: I don''t understand this: ~w~n', [Text]).
 execute_command(empty, _, empty) :-
-    format('No recipe loaded~n').
+    format('Error: no active recipe.~n').
 execute_command(ready(Meta, recipe(Title, Ingredients)),
                 add_ingredient(Ingredient),
                 ready(Meta, recipe(Title, NewIngredients))) :-
@@ -33,13 +38,14 @@ read_command(Command) :-
 
 print_state(empty) :-
     format('No recipe loaded.~n~n').
-print_state(state(ready(_, Recipe))) :-
+print_state(ready(_, Recipe)) :-
     print_recipe(Recipe).
 
 print_recipe(recipe(Name, Ingredients)) :-
-    format('%s~n~n', Name),
-    forall(member(i(Qty, Unit, Name), Ingredients),
-           format('  ~s ~s ~s~n', Qty, Unit, Name)).
+    format('~w~n~n', [Name]),
+    maplist(print_ingredient, Ingredients).
+print_ingredient(i(Qty, Unit, Name)) :-
+    format('  ~w ~w ~w~n', [Qty, Unit, Name]).
 
 main :-
     print_state(empty), main(empty).
@@ -47,6 +53,5 @@ main :-
 main(State) :-
     read_command(Command),
     execute_command(State, Command, NewState),
-    format('~w~n', NewState),
     print_state(NewState),
     main(NewState).
